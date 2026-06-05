238c5ff6502fc70273f3a929060af1997f5b3ea6
Mahmoud Hany <50390414+MahmoudHanyy@users.noreply.github.com>
2026-06-05 20:41:08 +0300

    codecrafters submit [skip ci]

--- app/stream.py
+++ app/stream.py
@@ -6,7 +6,17 @@ class Stream:
         self.stream = {}
         self.events = {}
 
+    def validate_id(self, id):
+        timestamp, seq = id.split(b'-')
+        if not (timestamp.isdigit() and seq.isdigit()) or (timestamp == b'0' and seq == b'0'):
+            raise ValueError("(error) ERR The ID specified in XADD must be greater than 0-0")
+        for key in self.stream.keys():
+            if timestamp <= self.stream[key].get('id', b'0-0') and seq <= self.stream[key].get('id', b'0-0'):
+                raise ValueError("(error) ERR The ID specified in XADD is equal or smaller than the target stream top item")
+
+
     def xadd(self, key, id, *values):
+        self.validate_id(id)
         if key not in self.stream:
             self.stream[key] = {'id': id}
         for i in range(0, len(values), 2):
