303943c0e2ef9d49bcf8fad50b59daa38cea9b77
Mahmoud Hany <50390414+MahmoudHanyy@users.noreply.github.com>
2026-06-05 21:16:46 +0300

    codecrafters submit [skip ci]

--- app/stream.py
+++ app/stream.py
@@ -6,18 +6,21 @@ class Stream:
         self.stream = {}
         self.events = {}
 
-    def validate_id(self, id):
+    def validate_id(self, key, id):
+        if key not in self.stream:
+            return
         timestamp, seq = id.split(b'-')
         if not (timestamp.isdigit() and seq.isdigit()) or (timestamp == b'0' and seq == b'0'):
             raise ValueError("(error) ERR The ID specified in XADD must be greater than 0-0")
-        for key in self.stream.keys():
-            if timestamp <= self.stream[key].get('id', b'0-0') and seq <= self.stream[key].get('id', b'0-0'):
+        for entry in self.stream[key].get('entries', []):
+            current_timestamp, current_seq = entry['id'].split(b'-')
+            if timestamp <= current_timestamp and seq <= current_seq:
                 raise ValueError("(error) ERR The ID specified in XADD is equal or smaller than the target stream top item")
 
-
     def xadd(self, key, id, *values):
-        self.validate_id(id)
+        self.validate_id(key, id)
         if key not in self.stream:
-            self.stream[key] = {'id': id}
-        for i in range(0, len(values), 2):
-            self.stream[key][values[i]] = values[i + 1]
+            self.stream[key] = {'entries': [{'id': id, 'values': values}]}
+        else:
+            for i in range(0, len(values), 2):
+                self.stream[key]['entries'].append({'id': id, 'values': values[i:i+2]})
